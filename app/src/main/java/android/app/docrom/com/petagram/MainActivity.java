12927ca16afbc52dd74db1f65b2ec9f186728ee3
alanmunguia <alanmunguia@outlook.com>
2016-06-12 20:53:49 -0500

    Petagram Semana 3

--- app/src/main/java/android/app/docrom/com/petagram/MainActivity.java
+++ app/src/main/java/android/app/docrom/com/petagram/MainActivity.java
@@ -1,13 +1,81 @@
 package android.app.docrom.com.petagram;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<Mascota> mascotas;
+    private RecyclerView listaMascotas;
+    private ImageView cincoEstrellas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar miActionbar = (Toolbar) findViewById(R.id.miActionBar);
+        setSupportActionBar(miActionbar);
+
+        cincoEstrellas = (ImageView) findViewById(R.id.ivCincoEstrellas);
+
+        listaMascotas = (RecyclerView) findViewById(R.id.rvMascotas);
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        llm.setOrientation(LinearLayoutManager.VERTICAL);
+        listaMascotas.setLayoutManager(llm);
+
+        inicializarListaMascotas();
+        inicializarAdaptador();
+        agregarFAB();
+
+
     }
+
+    public void agregarFAB(){
+        FloatingActionButton miFAB = (FloatingActionButton) findViewById(R.id.fabCamara);
+        miFAB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(v.getContext(),"Foto",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    public void inicializarListaMascotas(){
+        mascotas = new ArrayList<Mascota>();
+
+        mascotas.add(new Mascota(R.drawable.uno,"Lucky",6));
+        mascotas.add(new Mascota(R.drawable.dos,"Cooper",3));
+        mascotas.add(new Mascota(R.drawable.tres,"Rocky",2));
+        mascotas.add(new Mascota(R.drawable.cuatro,"Toby",5));
+        mascotas.add(new Mascota(R.drawable.cinco,"Dharma",4));
+    }
+
+    public void inicializarAdaptador(){
+        MascotaAdaptador adaptador = new MascotaAdaptador(mascotas);
+        listaMascotas.setAdapter(adaptador);
+    }
+
+    public void btnCincoEstrellas(View v){
+        Intent intent = new Intent(getApplicationContext(), Main2Activity.class);
+        startActivity(intent);
+    }
+
+
 }
